52c88daba313f5e94f58ea0ac74d90eec3cf9a7c
PavelSedoy <pavliksedoy@gmail.com>
2019-07-14 14:51:11 +0300

    React Router Dom + add Home, Websites and Not Found pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import WebSites from './pages/WebSites/WebSites'
+import NotFound from './pages/NotFound/NotFound'
 
 function App() {
   return (
-    <div>hello from app</div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/websites" component={WebSites} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
---
+++ src/pages/WebSites/WebSites.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WebSites = () => {
+  return (
+    <div>
+      from WebSites page
+    </div>
+  );
+};
+
+export default WebSites;
