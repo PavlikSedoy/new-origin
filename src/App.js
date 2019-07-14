3ce37801dbf3dd2579914097ee21b75e5f9e9086
PavelSedoy <pavliksedoy@gmail.com>
2019-07-14 18:02:22 +0300

    Transition Group

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from 'react'
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Transition, TransitionGroup } from 'react-transition-group'
 import Home from './pages/Home/Home'
 import WebSites from './pages/WebSites/WebSites'
 import NotFound from './pages/NotFound/NotFound'
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/websites" component={WebSites} />
-      <Route component={NotFound} />
-    </Switch>
+    <Route render={({location}) => {
+      const { pathname, key } = location;
+
+      return (
+        <>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/websites">Web Sites</Link>
+            <Link to="/error">Error</Link>
+          </nav>
+          <TransitionGroup component={null}>
+            <Transition
+              key={key}
+              appear={true}
+              // onEnter={}
+              // onExit={}
+              timeout={{enter: 750, exit: 750}}
+            >
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/websites" component={WebSites} />
+                <Route component={NotFound} />
+              </Switch>
+            </Transition>
+          </TransitionGroup>
+        </>
+      )
+    }} />
   );
 }
 
