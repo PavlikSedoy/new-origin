4b14e0f6a1106fd4568f7df10a115d8ab428b329
Pavel Sedoy <pavliksedoy@gmail.com>
2019-07-23 23:48:18 +0300

    Home screen bottom menu animation

--- frontend/src/components/Portfolio/Portfolio.js
+++ frontend/src/components/Portfolio/Portfolio.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import VisibilitySensor from "react-visibility-sensor";
+import { TweenMax, TimelineMax } from 'gsap'
+import PropTypes from 'prop-types'
 import style from './Portfolio.module.scss'
 
 class Portfolio extends Component {
   render() {
+    let visiblePortfolio = (isVisible) => {
+      var tlBottomMenu = new TimelineMax({})
+
+      if (isVisible) {
+        tlBottomMenu.staggerTo(".HeadNav_HeadNav__item__1DohS", .5, {height:0, opacity:0}, 0.2)
+      } else {
+        tlBottomMenu.staggerTo(".HeadNav_HeadNav__item__1DohS", .5, {height:100, opacity:1}, 0.2)
+      }
+    }
+
     return (
-      <section className={style.Portfolio}>
-        ddd
-      </section>
+      <VisibilitySensor minTopValue={100} partialVisibility='top'>
+        {({isVisible}) =>
+          <>
+            {visiblePortfolio(isVisible)}
+
+            <section className={style.Portfolio}>
+              ddd
+            </section>
+          </>
+        }
+      </VisibilitySensor>
     );
   }
 }
